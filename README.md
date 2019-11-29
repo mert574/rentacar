cbec7752838ce2c2920ab25f82444ff2632d8cdf
Mert Yildiz <mert574@gmail.com>
2019-11-29 22:02:50 +0300

    Refactors: Small fixes

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # Car Rental Project
 
+*Note:* For admin panel credentials, you can use `admin:admin`. However, it only checks whether the username and the password is same. So you can use anything you want.
+
+A pre-built version of this project is included in `/public` directory.
+
+All source files are in `/src`.
+
 ## Requirements
 - Node 12+
 - Yarn 1.19+
 
+## How to Build and Run?
+- Verify the requirements are installed on your system
+- Clone the repository
+- Use `yarn` or `yarn install` command to install dependencies.
+- Build the project with `yarn build` command. It will remove the contents of the public folder and builds in there.
+
 ## Commands
-- Install dependencies: ``yarn``
-- Start development server: ``yarn dev`` ( port `:8080` )
-- Build project: ``yarn build``
+- Install dependencies: `yarn`
+- Start development server: `yarn dev` ( port `:8080` )
+- Build project: `yarn build`
 ( builds to `/public` directory )
 
 ## Technologies used
--- src/checkout/checkout.js
+++ src/checkout/checkout.js
@@ -5,37 +5,37 @@ import fillTemplateWithCarInfo from "../fillTemplateWithCarInfo";
 
 $(() => {
     const {carId} = qs.parse(window.location.search.substring(1));
-    const {car, ...selection} = data.result[carId];
+    const {car} = data.result[carId];
 
-    console.log("checkout", car, selection);
     $("h4#carName").text(`${car.brand} ${car.name}`);
     $("img#carImg").attr("src", car.image);
 
-
     const template = `
-                <ul class="list-group">
-                    <li class="list-group-item my-1">
-                        <img src="/assets/icons/transmission.svg" alt="transmission type" height="20" data-toggle="tooltip" data-placement="left" title="Transmission Type">
-                        {{transmission}}
-                    </li>
-                    <li class="list-group-item my-1">
-                        <img src="/assets/icons/car.svg" alt="car class" height="20" data-toggle="tooltip" data-placement="left" title="Car Class">
-                        {{class}}
-                    </li>
-                    <li class="list-group-item my-1">
-                        <img src="/assets/icons/fuel.svg" alt="fuel type" height="20" data-toggle="tooltip" data-placement="left" title="Fuel Type">
-                        {{fuel}}
-                    </li>
-                    <li class="list-group-item my-1">
-                        <img src="/assets/icons/seat.svg" alt="seats" height="20" data-toggle="tooltip" data-placement="left" title="Seats">
-                        {{seat}} seats
-                    </li>
-                </ul>
-                <button type="button" class="my-2 btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
-                    Rent for
-                    <strong class="mx-1">{{price}}₺</strong>
-                    per day!
-                </button>
-        `;
+        <ul class="list-group">
+            <li class="list-group-item my-1">
+                <img src="/assets/icons/transmission.svg" alt="transmission type" height="20" data-toggle="tooltip" data-placement="left" title="Transmission Type">
+                {{transmission}}
+            </li>
+            <li class="list-group-item my-1">
+                <img src="/assets/icons/car.svg" alt="car class" height="20" data-toggle="tooltip" data-placement="left" title="Car Class">
+                {{class}}
+            </li>
+            <li class="list-group-item my-1">
+                <img src="/assets/icons/fuel.svg" alt="fuel type" height="20" data-toggle="tooltip" data-placement="left" title="Fuel Type">
+                {{fuel}}
+            </li>
+            <li class="list-group-item my-1">
+                <img src="/assets/icons/seat.svg" alt="seats" height="20" data-toggle="tooltip" data-placement="left" title="Seats">
+                {{seat}} seats
+            </li>
+        </ul>
+        <a href="/confirm">
+            <button type="button" class="my-2 btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+                Rent for
+                <strong class="mx-1">{{price}}₺</strong>
+                per day!
+            </button>
+        </a>
+    `;
     $("#carDetails").html(fillTemplateWithCarInfo(template, carId));
 });
